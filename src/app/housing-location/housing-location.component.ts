be85711f474baeafd019fed2b184d8395cd0ac3b
nwin19 <nandarwinbuma@gmail.com>
2024-02-16 21:21:22 +0630

    dynamic route

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,14 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
-  template: ` <p>housing-location works!</p> `,
+  imports: [RouterModule],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    </section>
+  `,
   styleUrl: './housing-location.component.css',
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
-}
+} // created a new property decorated with the @Input() decorator. You also used the non-null assertion operator to notify the compiler that the value of the new property won't be null or undefined
